bc0867e0426cde3fd14a778530b9fa7a8f8fc33b
Gabriel Santos <barricaspt@gmail.com>
2021-05-02 23:12:16 +0100

    Create transcode service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TranscodeModule } from './transcode/transcode.module';
 
 @Module({
-  imports: [],
+  imports: [TranscodeModule],
   controllers: [AppController],
   providers: [AppService],
 })
