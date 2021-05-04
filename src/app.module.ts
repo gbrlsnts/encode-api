52cf6cbe86f70bbd6f91d34f204794a1b6fbedba
Gabriel Santos <barricaspt@gmail.com>
2021-05-04 23:46:47 +0100

    Add typeorm and postgres config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { configSchema } from './config/validation';
 import { TranscodeModule } from './transcode/transcode.module';
 
@@ -18,6 +19,20 @@ import { TranscodeModule } from './transcode/transcode.module';
           host: configService.get<string>('REDIS_HOST'),
           port: configService.get<number>('REDIS_PORT'),
         },
+      }),
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PSWD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity.{js,ts}'],
+        synchronize: configService.get<string>('NODE_ENV') === 'development',
       }),
       inject: [ConfigService],
     }),
