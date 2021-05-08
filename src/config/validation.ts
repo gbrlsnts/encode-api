3e2760978419e77b964f3f0cb33109826e977525
Gabriel Santos <barricaspt@gmail.com>
2021-05-08 23:56:43 +0100

    Add more config validation checks

--- src/config/validation.ts
+++ src/config/validation.ts
@@ -1,9 +1,19 @@
 import * as Joi from '@hapi/joi';
 
+const requiredString = Joi.string().required().min(1);
+
 export const configSchema = Joi.object({
   NODE_ENV: Joi.string().valid('development', 'test', 'production'),
   APP_PORT: Joi.number().default(3000),
-  REDIS_HOST: Joi.string().required().min(1),
+  DB_HOST: requiredString,
+  DB_PORT: Joi.number().default(5432),
+  DB_USER: requiredString,
+  DB_PSWD: requiredString,
+  DB_NAME: requiredString,
+  REDIS_HOST: requiredString,
   REDIS_PORT: Joi.number().default(6379),
-  QUEUE_VIDEO: Joi.string().required().min(1),
+  QUEUE_PREFIX: Joi.string().min(1).default('bull'),
+  QUEUE_VIDEO: requiredString,
+  QUEUED_STATUS_CODE: requiredString,
+  FIRST_STATUS: requiredString,
 });
